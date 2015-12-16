7256a6c66971cad79f57677f863195eb18918c92
Peter Meissner <retep.meissner@gmail.com>
2015-12-16 08:27:59 +0100

    doc

--- R/tools.R
+++ R/tools.R
@@ -1,5 +1,5 @@
 #' make automatically named list
-#'
+#' @param ... things to be put in list
 named_list <- function(...){
   thelist <- list(...)
   names(thelist) <- as.character(substitute(list(...)))[-1]
