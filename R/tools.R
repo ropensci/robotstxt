1c3ced5e8b16b9140f9ce4b3b282d145a632bad5
Peter Meissner <retep.meissner@gmail.com>
2015-12-31 10:26:51 +0100

    loaders for build in robots.txt files

--- R/tools.R
+++ R/tools.R
@@ -5,3 +5,16 @@ named_list <- function(...){
   names(thelist) <- as.character(substitute(list(...)))[-1]
   thelist
 }
+
+#' load robots.txt files saved along with the package
+#' @param name name of the robots.txt files, defaults to a random drawn file ;-)
+#' @export
+rt_get_rtxt <- function(name=sample(rt_list_rtxt(),1)){
+  readLines( system.file( paste0("robotstxts/",name), package = "robotstxt" ), warn = FALSE)
+}
+
+#' list robots.txt files saved along with the package
+#' @export
+rt_list_rtxt <- function(){
+  list.files(system.file("robotstxts", package = "robotstxt" ))
+}
