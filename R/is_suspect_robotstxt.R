cd9376b2806a7a9a9f0167697401dc1ad85267cb
petermeissner <retep.meissner@gmail.com>
2018-11-19 10:36:52 +0100

    is_suspect_robotstxt()

--- R/is_suspect_robotstxt.R
+++ R/is_suspect_robotstxt.R
@@ -3,11 +3,27 @@
 #' function that checks if file is valid / parsable robots.txt file
 #'
 #' @param text content of a robots.txt file provides as character vector
-#' @export
 #'
 is_suspect_robotstxt <- function(text){
-  warning("needs implementation")
-  FALSE
+
+  # check for html
+  if( grep(x = text[1], pattern = "^\\s*<!doctype ", ignore.case = TRUE) ){
+    return(TRUE)
+  }
+
+  # check for xml
+  if( grep(x = text[1], pattern = "^\\s*<?xml ", ignore.case = TRUE) ){
+    return(TRUE)
+  }
+
+  # check for json
+  if( grep(x = text[1], pattern = "^\\s*{", ignore.case = TRUE) ){
+    return(TRUE)
+  }
+
+
+  # return default
+  return(FALSE)
 }
 
 
