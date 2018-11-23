f4f4a6b26b24485a53a90be3441b740eeb3c7683
petermeissner <retep.meissner@gmail.com>
2018-11-23 23:55:42 +0100

    tests vor http event handling - closes #34

--- tests/testthat/test_http_event_handling.R
+++ tests/testthat/test_http_event_handling.R
@@ -11,12 +11,87 @@ test_that("www redirects are handled silently", {
 })
 
 test_that("non www redirects are handled non silently", {
-  expect_silent({
-    www_redirect <- readRDS(system.file("http_requests/http_redirect_www.rds", package = "robotstxt"))
+  expect_warning({
+    domain_change <- readRDS(system.file("http_requests/http_domain_change.rds", package = "robotstxt"))
     get_robotstxt(
       "http://google.com",
-      rt_robotstxt_http_getter = function(...){www_redirect}
+      rt_robotstxt_http_getter = function(...){domain_change}
     )
   })
 })
 
+test_that("warn = FALSE does silences warnings", {
+  expect_silent({
+    domain_change <- readRDS(system.file("http_requests/http_domain_change.rds", package = "robotstxt"))
+    get_robotstxt(
+      "github.io",
+      rt_robotstxt_http_getter = function(...){domain_change},
+      warn = FALSE
+    )
+  })
+})
+
+
+test_that("suspect content", {
+  expect_true({
+    suppressWarnings({
+      suspect_content <- readRDS(system.file("http_requests/http_html_content.rds", package = "robotstxt"))
+      rtxt <-
+        get_robotstxt(
+          "pages.github.com",
+          rt_robotstxt_http_getter = function(...){suspect_content}
+        )
+      problems <- attr(rtxt, "problems")
+    })
+
+    !is.null(problems$on_file_type_mismatch) & problems$on_suspect_content$content_suspect
+  })
+})
+
+
+
+
+
+test_that("all ok", {
+  expect_silent({
+    http_ok <- readRDS(system.file("http_requests/http_ok_1.rds", package = "robotstxt"))
+    get_robotstxt(
+      "google.com",
+      rt_robotstxt_http_getter = function(...){http_ok}
+    )
+  })
+
+  expect_silent({
+    http_ok <- readRDS(system.file("http_requests/http_ok_2.rds", package = "robotstxt"))
+    get_robotstxt(
+      "google.com",
+      rt_robotstxt_http_getter = function(...){http_ok}
+    )
+  })
+
+  expect_silent({
+    http_ok <- readRDS(system.file("http_requests/http_ok_3.rds", package = "robotstxt"))
+    get_robotstxt(
+      "google.com",
+      rt_robotstxt_http_getter = function(...){http_ok}
+    )
+  })
+
+  expect_silent({
+    if ( Sys.getenv("rpkg_use_internet_for_testing") == "TRUE"  ){
+      get_robotstxt(
+        "google.com"
+      )
+    }
+  })
+
+
+  expect_silent({
+    if ( Sys.getenv("rpkg_use_internet_for_testing") == "TRUE"  ){
+      get_robotstxt(
+        "google.com",
+        force = TRUE
+      )
+    }
+  })
+})
